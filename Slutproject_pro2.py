37bd0a61740df548d4318780a207fa8b94e31cb7
Seg-Bert <TheMatteM1@gmail.com>
2022-05-08 20:05:21 +0200

    working on methods.

---
+++ Slutproject_pro2.py
@@ -0,0 +1,35 @@
+
+
+
+class Zoo:
+
+    def __init__(self, zone, type, animal, food):
+        self.zone = zone
+        self.type = type
+        self.animal = animal
+        self.food = food
+
+    def user_interface(self):
+        user_input = str(input(''))
+
+
+class Tropical_zone(Zoo):
+    
+    def __init__(self):
+        super.__init__()
+
+
+class Tempered_zone(Zoo):
+    
+    def __init__(self):
+        super.__init__()
+
+
+class Polar_zone(Zoo):
+    
+    def __init__(self):
+        super.__init__()
+
+
+
+# API   https://meowfacts.herokuapp.com/ 
